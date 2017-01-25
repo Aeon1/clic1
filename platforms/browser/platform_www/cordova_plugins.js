75a20514f8ab22be75488c2c2968e7cd4d3ae2f6
Aeon1 <joel.r@live.com.mx>
2017-01-25 16:20:41 -0700

    cambio

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
         "id": "cordova-plugin-fcm.FCMPlugin",
         "pluginId": "cordova-plugin-fcm",
@@ -20,10 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-console": "1.0.5",
-    "cordova-plugin-statusbar": "1.0.1",
-    "cordova-plugin-fcm": "2.1.1"
+    "cordova-plugin-fcm": "2.1.1",
+    "cordova-plugin-whitelist": "1.3.1"
 }
 // BOTTOM OF METADATA
 });
